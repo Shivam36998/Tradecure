56910ab0c60a2558900b958efb5cdecd14535faf
Harish Kumar <harishkumarnit03112002@gmail.com>
2023-11-05 01:06:17 +0530

    done changes

--- src/components/RootLayout/RootLayout.jsx
+++ src/components/RootLayout/RootLayout.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
-import Service from "../Services/ServiceMainPage/Service";
+import Navbar from "../Home1/Navbar/Navbar";
+import Footer from "../Home1/Footer/Footer";
+import Service from "../Home1/Services/ServiceMainPage/Service";
+import Consulting from "../Home1/Consulting/Consulting";
+import MarketArea from "../Home1/MarketArea/MarketArea";
+import NewsArea from "../Home1/NewsArea/NewsArea";
+import Testimonials from "../Home1/Testimonials/Testimonials";
 
 const RootLayout = () => {
   return (
@@ -10,6 +14,10 @@ const RootLayout = () => {
       <Navbar />
       <Outlet />
       <Service />
+      <Consulting />
+      <MarketArea />
+      <NewsArea />
+      <Testimonials />
       <Footer />
     </>
   );
